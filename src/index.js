29792bad263ca58aeb53172b9d35d23630fe0653
Matan Lieberman <matanlieberman@Matans-Mac-mini.local>
2017-06-16 15:30:12 +0300

    extract logic from main

--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
-import fileSystem from 'fs';
 import _ from 'lodash';
 import minimist from 'minimist';
 import id3Reader from 'node-id3';
-import Promise from 'bluebird';
+
+import readLibrary from './libraryReader';
 
 const { r, root } = minimist(process.argv.slice(2));
 const rootPath = root || r || process.env.MUSIC_FOLDER;
 
-const readdir = Promise.promisify(fileSystem.readdir);
-function readNoHidden(path) {
-  return readdir(path)
-    .then(content => _.filter(content, name => _.head(name) !== '.'));
-}
-
-console.log(rootPath)
-// Read all bands
-readNoHidden(rootPath)
-  // Read all bands albums
-  .map(bandName => Promise.props({
-    name: bandName,
-    path: `${rootPath}/${bandName}`,
-    __albums: readNoHidden(`${rootPath}/${bandName}`),
-  }))
-  // Read all songs in albums
-  .map(band => Promise.props(_.assign(band, {
-    albums: Promise.map(band.__albums, albumName => readNoHidden(`${band.path}/${albumName}`)
-      .then(songs => ({
-        name: albumName,
-        path: `${band.path}/${albumName}`,
-        songs,
-      })))
-  })))
+readLibrary(rootPath)
   .then(x => {
     console.log(JSON.stringify(x, null, '\t'));
   })
